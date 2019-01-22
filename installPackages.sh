b5fe58a2b65662aad41b7a76cc2af97a3a99d1f0
Sean McBride <spmcbride1201@gmail.com>
2019-01-22 18:08:07 -0500

    chore: uncomment apt update

--- installPackages.sh
+++ installPackages.sh
@@ -1,6 +1,9 @@
+# The intent here is to add a script for things I want to install between my UNIX environments
+# This isn't idempotent, so I don't want to run this directly yet...
+
 # Setup Ubuntu
-# sudo apt update --yes
-# sudo apt upgrade --yes
+sudo apt update --yes
+sudo apt upgrade --yes
 
 # Get Miniconda and make it the main python interpreter
 wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda-3-latest-Linux-x86_64.sh -O ~/miniconda.sh
@@ -9,3 +12,5 @@ rm ~/miniconda.sh
 
 # Install Antibody
 curl -sL git.io/antibody | sh -s
+
+# TODO: Install MPI... maybe just add binaries to repo and add to path?
