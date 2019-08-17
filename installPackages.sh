9bb93b6205eb74afa0a4ae0457bce69feae953d6
Sean McBride <spmcbride1201@gmail.com>
2019-08-17 14:03:41 -0400

    feat: make install script idempotent

--- installPackages.sh
+++ installPackages.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Useful resources since I don't write BASH scripts too often
+# https://arslan.io/2019/07/03/how-to-write-idempotent-bash-scripts/
+# https://tecadmin.net/tutorial/bash-scripting/
+
 # Setup Ubuntu
 sudo apt update --yes
 sudo apt upgrade --yes
@@ -13,7 +17,7 @@ if grep -q Microsoft /proc/version; then
   sudo rm -f /etc/fonts/local.conf
 fi
 
-# Install Fira Code for VSCode.
+# Install Fira Code for use in X Window Apps.
 sudo apt-get install fonts-firacode --yes
 
 # Install VIM
@@ -22,47 +26,83 @@ sudo apt-get install vim --yes
 # Install Curl
 sudo apt-get install curl --yes
 
-# Get Miniconda and make it the main python interpreter
-# Warning: Not idempotent
-# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh 
-# chmod +x ~/miniconda.sh
-# zsh ~/miniconda.sh -b -p ~/miniconda
-# rm ~/miniconda.sh
-# export PATH=$PATH:$HOME/miniconda/bin
-# conda update --prefix /home/sean/miniconda anaconda
-# conda create --name myenv --yes
-# source activate myenv
-
-# Install Fix for VSCode Python language server needed libssl 1.0.0
-# sudo apt-get install multiarch-support --yes
-# cd ~
-# wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb
-# sudo dpkg -i libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb
-
-# Install VSCode Extensions
-# code --install-extension CoenraadS.bracket-pair-colorizer
-# code --install-extension vscode-icons-team.vscode-icons
-# code --install-extension WakaTime.vscode-wakatime
-
-# code --install-extension ms-python.python
-# code --install-extension ms-python.anaconda-extension-pack
-# code --install-extension ms-vscode.cpptools
-# code --install-extension redhat.vscode-yaml
-# code --install-extension vscjava.vscode-java-pack
-# code --install-extension davidanson.vscode-markdownlint
-# code --install-extension redhat.vscode-xml
-
-# Install Android Malware Tools
-# pip install -U androguard pyqt5 pyperclip
-# sudo apt-get install qtdeclarative5-dev
-
 # Install Antibody
-curl -sL git.io/antibody | sh -s
+if [ -x "$(command -v antibody)" ]
+then
+  antibody update
+else 
+  curl -sL git.io/antibody | sh -s
+fi
 
 # Install C / C++ tools
 sudo apt-get install gcc gdb g++ clang-format make libtinfo5 libopenmpi-dev --yes
 
-## Install OpenMPI from Source... This is SLOW!
+# Install Rust Tools
+if [ -x "$(command -v rustup)" ]
+then
+  rustup update
+else 
+  curl https://sh.rustup.rs -sSf | sh
+fi
+rustup component add rustfmt
+rustup component add clippy
+
+# Add Java tools
+sudo apt-get install openjdk-11-jdk openjdk-8-jdk maven --yes
+if [ ! -d "~/.jenv"]; then
+  git clone https://github.com/gcuisinier/jenv.git ~/.jenv
+  source ~/.zshrc
+fi
+jenv add /usr/lib/jvm/java-8-openjdk-amd64
+jenv add /usr/lib/jvm/java-11-openjdk-amd64
+jenv global 11.0
+
+# Install N, Node, and the Native Module build tools 
+# Note: you need to remove the N_PREFIX value from zshrc prior to installation
+# n-install: ERROR:
+#   Aborting, because an existing definition of $N_PREFIX added by someone else
+#   was found in '/home/sean/.zshrc':
+
+#   export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
+#   Please remove it and try again.
+# n-install: ERROR: ABORTING due to unexpected error.
+if [ -x "$(command -v n)" ]
+then
+  n-update -y
+else 
+  curl -L https://git.io/n-install | bash
+  source ~/.zshrc
+fi
+
+if ! [ -x "$(command -v http-server)" ]; then
+  npm i -g http-server
+fi
+
+if ! [ -x "$(command -v netlify)" ]; then
+  npm i -g netlify-cli
+fi
+
+if ! [ -x "$(command -v fkill)" ]; then
+  npm i -g fkill-cli
+fi
+
+if ! [ -x "$(command -v rimraf)" ]; then
+  npm i -g rimraf
+fi
+
+## Auto-remove stuff I don't need
+sudo apt autoremove -y
+
+## TODO Stuff
+
+# Move to zshrc
+# sudo echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+
+# Alias for colonialzero
+# ssh seanmcbride@login.colonialone.gwu.edu
+
+## Backup.... Install OpenMPI from Source... This is SLOW!
 # cd ~
 # wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.3.tar.gz
 # tar -xvf openmpi-3.1.3.tar.gz
@@ -81,45 +121,23 @@ sudo apt-get install gcc gdb g++ clang-format make libtinfo5 libopenmpi-dev --ye
 # ./emsdk install sdk-1.38.15-64bit
 # ./emsdk activate sdk-1.38.15-64bit
 
-# Install Rust Tools
-curl https://sh.rustup.rs -sSf | sh
-rustup component add rustfmt
-rustup component add clippy
+# Install Fix for VSCode Python language server needed libssl 1.0.0
+# sudo apt-get install multiarch-support --yes
+# cd ~
+# wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb
+# sudo dpkg -i libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb
 
-# Add Java tools
-sudo apt-get install openjdk-11-jdk openjdk-8-jdk maven --yes
-git clone https://github.com/gcuisinier/jenv.git ~/.jenv
-source ~/.zshrc
-jenv add /usr/lib/jvm/java-8-openjdk-amd64
-jenv add /usr/lib/jvm/java-11-openjdk-amd64
-jenv global 11.0
+# Install Android Malware Tools
+# pip install -U androguard pyqt5 pyperclip
+# sudo apt-get install qtdeclarative5-dev
 
-# Install N, Node, and the Native Module build tools 
-# Note: you need to remove the N_PREFIX value from zshrc prior to installation
-# n-install: ERROR:
-#   Aborting, because an existing definition of $N_PREFIX added by someone else
-#   was found in '/home/sean/.zshrc':
-
-#   export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
-
-#   Please remove it and try again.
-# n-install: ERROR: ABORTING due to unexpected error.
-curl -L https://git.io/n-install | bash
-# Source in order to have Node and npm in path for global installs
-source ~/.zshrc
-npm i -g http-server
-npm i -g netlify-cli
-npm i -g fkill-cli
-npm i -g rimraf
-
-
-## Auto-remove stuff I don't need
-sudo apt autoremove -y
-
-## TODO Stuff
-
-# Move to zshrc
-# sudo echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-
-# Alias for colonialzero
-# ssh seanmcbride@login.colonialone.gwu.edu
+# Get Miniconda and make it the main python interpreter
+# Warning: Not idempotent
+# wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh 
+# chmod +x ~/miniconda.sh
+# zsh ~/miniconda.sh -b -p ~/miniconda
+# rm ~/miniconda.sh
+# export PATH=$PATH:$HOME/miniconda/bin
+# conda update --prefix /home/sean/miniconda anaconda
+# conda create --name myenv --yes
+# source activate myenv
