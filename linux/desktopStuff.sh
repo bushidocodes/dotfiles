37bea164230b3ccb88c00b880de96590c3402c0b
Sean McBride <spmcbride1201@gmail.com>
2020-01-02 15:27:16 -0500

    chore: Update Desktop install script for Linux

--- linux/desktopStuff.sh
+++ linux/desktopStuff.sh
@@ -1,6 +1,9 @@
+# Desktop Apps
 sudo snap install code --classic
 sudo snap install slack --classic
+sudo snap install teams-for-linux
 
+# VSCode Extensions
 code --install-extension PKief.material-icon-theme
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension stardog-union.stardog-rdf-grammars
@@ -21,9 +24,24 @@ code --install-extension serayuzgur.crates
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension WakaTime.vscode-wakatime
 
-# Docker
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt install docker.io
+# Docker CE
+# Based on https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
