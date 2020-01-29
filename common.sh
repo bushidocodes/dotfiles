9561f765241995e990ea4cb4026b8265a60c4815
Sean McBride <spmcbride1201@gmail.com>
2020-01-28 21:45:02 -0500

    refactor: clean up look and feel func

--- common.sh
+++ common.sh
@@ -68,17 +68,17 @@ configure_look_and_feel() {
 
   # enable color support of ls and also add handy aliases
   if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    if test -r ~/.dircolors; then
+      eval "$(dircolors -b ~/.dircolors)"
+    else
+      eval "$(dircolors -b)"
+    fi
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
-
-    alias bats='bats *_test.sh'
-    alias batsall='BATS_RUN_SKIPPED=true bats'
   fi
 
   # colored GCC warnings and errors
@@ -116,6 +116,9 @@ configure_aliases() {
   # Add an "alert" alias for long running commands.  Use like so:
   #   sleep 10; alert
   alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+  alias bats='bats *_test.sh'
+  alias batsall='BATS_RUN_SKIPPED=true bats'
 
   configure_mac_style_open_alias
 }
