87ca95aab63ef54dfb38288ebefa57db1d621ffd
Sean McBride <spmcbride1201@gmail.com>
2020-10-09 20:41:54 -0400

    chore: add WSL clip alias

--- common.sh
+++ common.sh
@@ -112,7 +112,9 @@ configure_aliases() {
   alias ll='ls -alF'
   alias la='ls -A'
   alias l='ls -CF'
-
+  if is_wsl; then
+    alias clip='clip.exe'
+  fi
   # Add an "alert" alias for long running commands.  Use like so:
   #   sleep 10; alert
   alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
